41bc2d2f7d8e89ee590ef5ab4ecd9a980c795d91
Lígia Mizuyama <79666957+limiyama@users.noreply.github.com>
2025-09-01 11:50:11 -0300

    etc

--- data_analysis.py
+++ data_analysis.py
@@ -1,14 +1,21 @@
-# count NRPS, PKS or NRPS-PKS
 import pandas as pd
 
 df = pd.read_csv('BGC_summary_all.tsv', sep='\t')
-column_data = df['BGC_Class'].astype(str) 
 
-nrps = column_data.str.count('NRPS').sum() - column_data.str.count('NRPS-PKS').sum()
-pks = column_data.str.count('PKS').sum() - column_data.str.count('NRPS-PKS').sum()
-nrps_pks = column_data.str.count('NRPS-PKS').sum()
+# count NRPS, PKS or NRPS-PKS
+column_bgc_class = df['BGC_Class'].astype(str) 
+
+nrps = column_bgc_class.str.count('NRPS').sum() - column_bgc_class.str.count('NRPS-PKS').sum()
+pks = column_bgc_class.str.count('PKS').sum() - column_bgc_class.str.count('NRPS-PKS').sum()
+nrps_pks = column_bgc_class.str.count('NRPS-PKS').sum()
 
 print("NRPS: ", nrps, "\nPKS: ", pks, "\nNRPS-PKS: ", nrps_pks)
 
+# count how many BGCs scored higher than jamaicamides, hectochlorin, etc
+jamaicamides = (df["Score"]>91.5).sum()
+hectochlorin = (df["Score"]>55).sum()
+cryptomaldamide = (df["Score"]>42).sum()
 
-# count how many BGCs scored higher than the jamaicamides
+print("Number of BGCs that scored higher than jamaicamides: ", jamaicamides)
+print("Number of BGCs that scored higher than hectochlorin: ", hectochlorin)
+print("Number of BGCs that scored higher than cryptomaldamide: ", cryptomaldamide)
